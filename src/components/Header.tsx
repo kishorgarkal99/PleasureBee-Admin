6d8b68b19a6f5dad856b72c02ff57193ae9e6fe2
fikireabm <fikireab.mekuriaw@kgp2ssindia.in>
2023-08-22 14:08:45 +0300

    some color correction

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,8 @@ type HeaderProp = {
 }
 const Header = ({ title }: HeaderProp) => {
     return (
-        <div className="w-full bg-black h-16 px-16 text-white flex items-center">{title}</div>
+        <div className="m-2 bg-gradient-to-r from-pink-700 to-blue-400 px-24 py-6 rounded-md shadow-xl text-xl font-bold text-white flex items-center">{title}
+        </div>
     )
 }
 
