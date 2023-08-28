9cc8695b08376ae671d93d41eabebf2c0b8ee1d0
fikireabm <fikireab.mekuriaw@kgp2ssindia.in>
2023-08-28 13:19:00 +0300

    exported environment variables

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import {getFirestore} from "firebase/firestore"
 // import { getAnalytics } from "firebase/analytics";
 const firebaseConfig = {
-  apiKey: "AIzaSyDhL2hpiDuj39s9DrsCloAF8Kcs0zZaxAc",
-  authDomain: "pleasurebee-2.firebaseapp.com",
-  projectId: "pleasurebee-2",
-  storageBucket: "pleasurebee-2.appspot.com",
-  messagingSenderId: "127813979231",
-  appId: "1:127813979231:web:c3712ce29d34baf1a21fea",
-  measurementId: "G-P9XH4MEXCD"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGE_SENDER_ID,
+  appId: import.meta.env.VITE_APPID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
