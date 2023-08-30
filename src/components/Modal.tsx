9c60835a544cbfb2f189167d114a9a86cfe07c8e
fikireabm <fikireab.mekuriaw@kgp2ssindia.in>
2023-08-30 16:11:40 +0300

    added editable functionality to plans

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,9 +1,12 @@
+import { FaCrown, FaInfo, FaTrashAlt } from "react-icons/fa";
+import { IconButton } from "./Widgets";
+
 type ModalProp = {
     showModal: boolean
-    setShowModal:React.Dispatch<React.SetStateAction<boolean>>
+    setShowModal: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-export default function Modal({ showModal, setShowModal}: ModalProp) {
+export default function Modal({ showModal, setShowModal }: ModalProp) {
     return (
         <>
             {showModal ? (
@@ -15,46 +18,49 @@ export default function Modal({ showModal, setShowModal}: ModalProp) {
                             {/*content*/}
                             <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                                 {/*header*/}
-                                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                                    <h3 className="text-3xl font-semibold">
-                                        Modal Title
-                                    </h3>
-                                    <button
-                                        className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                                        onClick={() => setShowModal(false)}
-                                    >
-                                        <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                                            ×
-                                        </span>
+                                <div className="flex items-center justify-between p-4 border-b border-solid border-slate-400 rounded-t">
+                                    <div className="flex justify-start">
+                                        <FaCrown className="w-6 h-6 text-pink-700 -rotate-45 -translate-y-2" />
+                                        <h3 className="text-2xl text-pink-500 font-semibold">
+                                            Premium
+                                        </h3>
+                                    </div>
+                                    <button onClick={() => alert("Are you sure ?")} className="rounded-full p-2 outline-0 ring-0 border-2 text-pink-500 border-pink-500 hover:text-white hover:bg-pink-500">
+                                        <FaTrashAlt className="w-4 h-4" />
                                     </button>
                                 </div>
                                 {/*body*/}
-                                <div className="relative p-6 flex-auto">
-                                    <p className="my-4 text-slate-500 text-lg leading-relaxed">
-                                        I always felt like I could do anything. That’s the main
-                                        thing people are controlled by! Thoughts- their perception
-                                        of themselves! They're slowed down by their perception of
-                                        themselves. If you're taught you can’t do anything, you
-                                        won’t do anything. I was taught I could do everything.
+                                <div className="relative p-4 flex-col">
+                                    <p className="text-slate-500 text-lg leading-relaxed">
+                                        <FaInfo className="mr-1 text-gray-600 inline text-base" />
+                                        <span>
+                                            Write the features this plan supports, and use comma to separate each feature.
+                                        </span>
                                     </p>
+                                    <div className="mb-3 pt-0">
+                                        <textarea
+                                            rows={4}
+                                            placeholder="Placeholder"
+                                            className="mt-2 px-3 py-4 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded-md text-base border-2 border-gray-300 shadow outline-none focus:outline-none focus:ring-0 w-full" />
+                                    </div>
                                 </div>
                                 {/*footer*/}
                                 <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
-                                            <button
-                                                className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-6 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
-                                                type="button"
-                                                onClick={()=>setShowModal(false)}
-                                            >
-                                                Close
-                                            </button>
-                                            <button
-                                                className="inline-flex w-full justify-center rounded-lg bg-pink-700 px-6 py-2 text-sm font-semibold text-white shadow-sm hover:bg-pink-500 sm:ml-3 sm:w-auto"
-                                                type="button"
-                                                onClick={()=>setShowModal(false)}
-                                            >
-                                                Save Changes
-                                            </button>
-                                        </div>
+                                    <button
+                                        className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-6 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                                        type="button"
+                                        onClick={() => setShowModal(false)}
+                                    >
+                                        Close
+                                    </button>
+                                    <button
+                                        className="inline-flex w-full justify-center rounded-lg bg-pink-700 px-6 py-2 text-sm font-semibold text-white shadow-sm hover:bg-pink-500 sm:ml-3 sm:w-auto"
+                                        type="button"
+                                        onClick={() => setShowModal(false)}
+                                    >
+                                        Save Changes
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     </div>
