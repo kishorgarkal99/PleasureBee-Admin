f32679e2292af7f44646c73c7c8aba3b166f1966
fikireabm <fikireab.mekuriaw@kgp2ssindia.in>
2023-09-04 09:51:20 +0300

    shifted to pleasurebee dev

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -11,13 +11,21 @@ const firebaseConfig = {
   // appId: import.meta.env.VITE_APPID,
   // measurementId: import.meta.env.VITE_MEASUREMENT_ID
 
-  apiKey: "AIzaSyDhL2hpiDuj39s9DrsCloAF8Kcs0zZaxAc",
-  authDomain: "pleasurebee-2.firebaseapp.com",
-  projectId: "pleasurebee-2",
-  storageBucket: "pleasurebee-2.appspot.com",
-  messagingSenderId: "127813979231",
-  appId: "1:127813979231:web:c3712ce29d34baf1a21fea",
-  measurementId: "G-P9XH4MEXCD"
+  // apiKey: "AIzaSyDhL2hpiDuj39s9DrsCloAF8Kcs0zZaxAc",
+  // authDomain: "pleasurebee-2.firebaseapp.com",
+  // projectId: "pleasurebee-2",
+  // storageBucket: "pleasurebee-2.appspot.com",
+  // messagingSenderId: "127813979231",
+  // appId: "1:127813979231:web:c3712ce29d34baf1a21fea",
+  // measurementId: "G-P9XH4MEXCD"
+
+  apiKey: "AIzaSyAbugS-RC7t-voHYLot3zR38ycSJbNnOek",
+  authDomain: "pleasurebee-dev.firebaseapp.com",
+  projectId: "pleasurebee-dev",
+  storageBucket: "pleasurebee-dev.appspot.com",
+  messagingSenderId: "1043807013452",
+  appId: "1:1043807013452:web:2de046b56cba6ac95102e9",
+  measurementId: "G-H3KT92BM4G"
 };
 
 // Initialize Firebase
